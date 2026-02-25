f6af750e11793aecf58891fc1d8d7c8d79d1f14b
Maksym-UA <max.savin3@gmail.com>
2026-02-25 15:43:46 +0200

    The sensor toggles the LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,41 @@
 #include <Arduino.h>
 
-#define BUTTON_PIN 17
+#define ADC_PIN 4
+#define VREF 3.0
+#define ADC_RESOLUTION 4095.0
+#define LED_PIN 16
+#define VOLTAGE_THRESHOLD 1.5
+#define VOLTAGE_GIST 2.0
 
-
-int16_t counter = 0;
-
-void IRAM_ATTR reaction() {
-  counter++;
-  Serial.println("\nButton Pressed! Count: " + String(counter));
+float getVoltage(int adcValue){
+  //convert ADC value to voltage on the sensor
+  return ((adcValue / ADC_RESOLUTION) * VREF);
 }
-
-
-
-void setup() {
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+void setup(){
+  //start the serial monitor at baude rate of 115200
   Serial.begin(115200);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), reaction, FALLING);
-
+  pinMode (LED_PIN, OUTPUT); //init pin 16 as output
+  analogReadResolution(12); //set the ADC resolution to 12 bits (0-4095)
+  Serial.println("Setup is ready!");
+  
 }
 
-void loop() {
-    if (digitalRead(BUTTON_PIN == LOW)) {
-        Serial.println("Button pressed!");
-        delay(50);
 
-        while(digitalRead(BUTTON_PIN == LOW)){
-            delay(10);
-        }
-    }
-    
-    Serial.println("Button pressed!");
-    delay(150);
+void loop(){
+  int adcValue = 0;
+  adcValue = analogRead(ADC_PIN); //read the ADC value on the pin
+  float voltage = getVoltage(adcValue); //convert ADC value to voltage
+  Serial.printf("ADC value: %d, Voltage %.2f V\n",  adcValue, voltage);
+  digitalWrite(LED_PIN, HIGH);
+
+  if (voltage < VOLTAGE_THRESHOLD ){
+    digitalWrite(LED_PIN, HIGH); //turn on the led if the voltage is below the threshold
+    Serial.println("The LED is on. Voltage is below threshold.");
+  }
+  else if (voltage > VOLTAGE_THRESHOLD){
+    digitalWrite(LED_PIN, LOW); //turn on the led if the voltage is below the threshold
+    Serial.println("The LED is off. Voltage is above threshold.");
+  } 
+
+  delay(200);
 }
