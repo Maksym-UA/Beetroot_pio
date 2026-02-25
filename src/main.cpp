38322c3da94f7f16310a06de4ea227c95e87cb91
Maksym-UA <max.savin3@gmail.com>
2026-02-25 23:30:18 +0200

    The lesson is complete

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,21 @@ float getVoltage(int adcValue){
   //convert ADC value to voltage on the sensor
   return ((adcValue / ADC_RESOLUTION) * VREF);
 }
+
+
 void setup(){
   //start the serial monitor at baude rate of 115200
   Serial.begin(115200);
   pinMode (LED_PIN, OUTPUT); //init pin 16 as output
   analogReadResolution(12); //set the ADC resolution to 12 bits (0-4095)
   Serial.println("Setup is ready!");
+
+  //analogSetPinAttenuation(4, ADC_0db); // Set attenuation for GPIO 4
+  //analogSetPinAttenuation(4, ADC_2_5db); // Set attenuation for GPIO 4
+  //analogSetPinAttenuation(4, ADC_6db); // Set attenuation for GPIO 4
+  analogSetPinAttenuation(4, ADC_11db); // Set attenuation for GPIO 4
+
+
   
 }
 
@@ -25,17 +34,23 @@ void loop(){
   int adcValue = 0;
   adcValue = analogRead(ADC_PIN); //read the ADC value on the pin
   float voltage = getVoltage(adcValue); //convert ADC value to voltage
-  Serial.printf("ADC value: %d, Voltage %.2f V\n",  adcValue, voltage);
-  digitalWrite(LED_PIN, HIGH);
+  uint32_t millivolts = analogReadMilliVolts(4); //read the voltage in millivolts
+  float error = (1 - ((voltage * 1000) / millivolts)) * 100;
+
+  
+  Serial.printf(
+    "ADC value: %d, Voltage %.2f V, Millivolts %u mV----Measurement Error: %.2f%%----\n",  
+    adcValue, voltage, millivolts, error);
+
 
   if (voltage < VOLTAGE_THRESHOLD ){
     digitalWrite(LED_PIN, HIGH); //turn on the led if the voltage is below the threshold
-    Serial.println("The LED is on. Voltage is below threshold.");
+    //Serial.println("The LED is on. Voltage is below threshold.");
   }
   else if (voltage > VOLTAGE_THRESHOLD){
     digitalWrite(LED_PIN, LOW); //turn on the led if the voltage is below the threshold
-    Serial.println("The LED is off. Voltage is above threshold.");
+    //Serial.println("The LED is off. Voltage is above threshold.");
   } 
 
-  delay(200);
+  delay(100);
 }
