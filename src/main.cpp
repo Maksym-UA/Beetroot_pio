310ba3e5f5c1c99d7d3a104aef9d7b91a33af6cf
Maksym-UA <max.savin3@gmail.com>
2026-02-28 00:12:03 +0200

    Complete the project with external  LED strip

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,11 @@
 #define VREF 3.0
 #define ADC_RESOLUTION 4095.0
 #define LED_PIN 16
-#define VOLTAGE_THRESHOLD 1.5
-#define VOLTAGE_GIST 2.0
+// Налаштування гістерезису
+const int darkThreshold = 1200;  // Turn ON when it gets this dark
+const int lightThreshold = 1800; // Turn OFF only when it gets this bright
+
+bool relayState = false; // Track current state (false = OFF, true = ON)
 
 float getVoltage(int adcValue){
   //convert ADC value to voltage on the sensor
@@ -14,43 +17,38 @@ float getVoltage(int adcValue){
 }
 
 
-void setup(){
+void setup() {
   //start the serial monitor at baude rate of 115200
   Serial.begin(115200);
   pinMode (LED_PIN, OUTPUT); //init pin 16 as output
   pinMode(RELAY_PIN, OUTPUT); //set pin 17 as relay output
-  digitalWrite(RELAY_PIN, LOW); //set original state of the relay to low
+  digitalWrite(RELAY_PIN, HIGH); // Default OFF
   analogReadResolution(12); //set the ADC resolution to 12 bits (0-4095)
   Serial.println("Setup is ready!");
-
 }
 
-
-void loop(){
+void loop() {
   int adcValue = 0;
   adcValue = analogRead(ADC_PIN); //read the ADC value on the pin
   float voltage = getVoltage(adcValue); //convert ADC value to voltage
-  uint32_t millivolts = analogReadMilliVolts(4); //read the voltage in millivolts
-  float error = (1 - ((voltage * 1000) / millivolts)) * 100;
-
-  
   Serial.printf(
-    "ADC value: %d, Voltage %.2f V, Millivolts %u mV----Measurement Error: %.2f%%----\n",  
-    adcValue, voltage, millivolts, error);
+    "ADC value: %d, Voltage %.2f V\n", adcValue, voltage);
+ 
 
-
-  if (voltage < VOLTAGE_THRESHOLD ){
+  if (!relayState && adcValue < darkThreshold) {
+    // If it was OFF and gets dark enough, turn it ON
+    digitalWrite(RELAY_PIN, LOW); 
     digitalWrite(LED_PIN, HIGH); //turn on the led if the voltage is below the threshold
-    digitalWrite(RELAY_PIN, HIGH); //activate the relay when it gets dark
-    Serial.println("The LED is on. Voltage is below threshold.");
-    Serial.println("The relay is on.");
-  }
-  else if (voltage > VOLTAGE_THRESHOLD){
-    digitalWrite(LED_PIN, LOW); //turn off the led if the voltage is below the threshold
-    digitalWrite(RELAY_PIN, LOW); //turn off the relay when it is bright
-    Serial.println("The LED is off. Voltage is above threshold.");
-    Serial.println("The relay is off.");
+    relayState = true;
+    Serial.println("Relay Triggered: ON");
   } 
+  else if (relayState && adcValue > lightThreshold) {
+    // If it was ON and gets bright enough, turn it OFF
+    digitalWrite(RELAY_PIN, HIGH); 
+    digitalWrite(LED_PIN, LOW); //turn on the led if the voltage is below the threshold
+    relayState = false;
+    Serial.println("Relay Triggered: OFF");
+  }
 
-  delay(200);
+  delay(200); // Short delay for stability
 }
